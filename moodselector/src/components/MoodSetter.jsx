378c8a39a93aa29d9c08ade4593975ccc70970f6
Talha Akram <talhaakram567i@gmail.com>
2025-10-13 05:35:58 -0700

    ok

---
+++ moodselector/src/components/MoodSetter.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const MoodSetter = ({mood}) => {
+  return (
+    <div>
+        <button>Mood is {mood}</button>
+    <div className='quote'>💡 HAPPINES IS A CHOICE AND POISTIVITY IS MINDSET </div>
+    </div>
+  )
+}
+
+export default MoodSetter
